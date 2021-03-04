5e605395ae7f613d18329673dcd911ab5dd59354
Kenneth López <khlopez2000@gmail.com>
2021-03-04 14:03:31 -0600

    Recorridos de arbol binario

--- main.go
+++ main.go
@@ -2,92 +2,105 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
-type nodo struct {
-	nombre, apellido, apodo, favoritos string
-	Siguiente, Anterior                *nodo
+type Arbol struct {
+	raiz *Nodo
 }
 
-type lista struct {
-	cabeza *nodo
-	cola   *nodo
+type Nodo struct {
+	dato     int
+	der, izq *Nodo
 }
 
-func (this *lista) Insertar(nuevo *nodo) {
-	if this.cabeza == nil {
-		this.cabeza = nuevo
-		this.cola = nuevo
+func NewArbol() *Arbol {
+	return &Arbol{raiz: nil}
+}
+
+func NewNodo(dato int) *Nodo {
+	return &Nodo{dato: dato, der: nil, izq: nil}
+}
+
+func (arbol *Arbol) Insertar(dato int) {
+	arbol.raiz = Insertar(dato, arbol.raiz)
+}
+
+func Insertar(dato int, nodo *Nodo) *Nodo {
+	if nodo == nil {
+		return NewNodo(dato)
+	}
+	if dato < nodo.dato {
+		nodo.izq = Insertar(dato, nodo.izq)
 	} else {
-		this.cola.Siguiente = nuevo
-		nuevo.Anterior = this.cola
-		this.cola = nuevo
+		nodo.der = Insertar(dato, nodo.der)
 	}
+	return nodo
 }
 
-func GetAscii(texto string) int {
-	var ascii int
-	runes := []rune(texto)
-	for i := 0; i < len(runes); i++ {
-		ascii += int(runes[i])
+func PreOrden(raiz *Nodo) string {
+	var cadena strings.Builder
+	if raiz == nil {
+		return ""
 	}
-	return ascii
+	cadena.WriteString(strconv.Itoa(raiz.dato))
+	cadena.WriteString(", ")
+	cadena.WriteString(PreOrden(raiz.izq))
+	cadena.WriteString(PreOrden(raiz.der))
+	return cadena.String()
+}
+
+func InOrden(raiz *Nodo) string {
+	var cadena strings.Builder
+	if raiz == nil {
+		return ""
+	}
+	cadena.WriteString(InOrden(raiz.izq))
+	cadena.WriteString(strconv.Itoa(raiz.dato))
+	cadena.WriteString(", ")
+	cadena.WriteString(InOrden(raiz.der))
+	return cadena.String()
+}
+
+func PostOrden(raiz *Nodo) string {
+	var cadena strings.Builder
+	if raiz == nil {
+		return ""
+	}
+	cadena.WriteString(PostOrden(raiz.izq))
+	cadena.WriteString(PostOrden(raiz.der))
+	cadena.WriteString(strconv.Itoa(raiz.dato))
+	cadena.WriteString(", ")
+	return cadena.String()
+}
+
+func (arbol *Arbol) Recorrer() string {
+	var cadena strings.Builder
+	cadena.WriteString("***RECORRIDO PREORDEN***\n")
+	cadena.WriteString(PreOrden(arbol.raiz))
+	cadena.WriteString("\n\n")
+	cadena.WriteString("***RECORRIDO INORDEN***\n")
+	cadena.WriteString(InOrden(arbol.raiz))
+	cadena.WriteString("\n\n")
+	cadena.WriteString("***RECORRIDO POSTORDEN***\n")
+	cadena.WriteString(PostOrden(arbol.raiz))
+	cadena.WriteString("\n\n")
+	return cadena.String()
 }
 
 func main() {
-	/*li := lista{nil, nil}
-	a := nodo{"Marvin", "Martinez", "Marvin25ronal", "Jugar apex", nil, nil}
-	b := nodo{"Yaiza", "Pineda", "Bambi", "Patinar", nil, nil}
-	c := nodo{"Jonathan", "Lopez", "Pancho", "Comer", nil, nil}
-	d := nodo{"usuario1", "bla", "bla", "Jugar apex", nil, nil}
-	e := nodo{"usuario2", "bla", "bla", "Jugar apex", nil, nil}
-	f := nodo{"usuario3", "sale edd", "vamos con todo", "100 en la fase 1", nil, nil}
-	li.Insertar(&a)
-	li.Insertar(&b)
-	li.Insertar(&c)
-	li.Insertar(&d)
-	li.Insertar(&e)
-	li.Insertar(&f)
-	li.graficar()*/
-	var name string = "Báez y Monroy"
-	fmt.Println(GetAscii(name))
-	var ascii int
-	for i := 0; i < len(name); i++ {
-		ascii += int(name[i])
-	}
-	fmt.Println(ascii)
+	arbol := NewArbol()
+	arbol.Insertar(5)
+	arbol.Insertar(8)
+	arbol.Insertar(2)
+	arbol.Insertar(1)
+	arbol.Insertar(4)
+	arbol.Insertar(3)
+	arbol.Insertar(9)
+	arbol.Insertar(10)
+	arbol.Insertar(6)
+	arbol.Insertar(7)
+	fmt.Println(arbol.Recorrer())
+
 }
-
-/*func (this *lista) graficar() {
-	var codigo, conexiones string
-	var contador int = 0
-	codigo = "digraph G{\n"
-	if this.cabeza != nil {
-		aux := this.cabeza
-		for aux != nil {
-			codigo += "nodo" + strconv.Itoa(contador) + "[shape=Mrecord, color=" +
-				"\"#00bf0d\" label=\"{{" + aux.nombre + " " + aux.apellido + "|" +
-				aux.apodo + "}|" + aux.favoritos + "}\" color=\"#80036b\"" + "];\n"
-			if contador > 0 {
-				conexiones += "nodo" + strconv.Itoa(contador-1) + "->nodo" +
-					strconv.Itoa(contador) + "[arrowhead=rvee, color=yellow]\n"
-				conexiones += "nodo" + strconv.Itoa(contador) + "->nodo" +
-					strconv.Itoa(contador-1) + "[arrowhead=rvee, color=orange]\n"
-			}
-			contador++
-			aux = aux.Siguiente
-		}
-		codigo += "head [shape = oval label = \"Cabeza\" color  = \"#000dff\"]\n" +
-			"cola [shape = oval, label = \"Cola\" color  = \"#000dff\"]\n"
-		conexiones += "head->nodo0 [arrowhead=vee, color=red]\n" +
-			"nodo" + strconv.Itoa(contador-1) + "-> cola [dir=back arrowtail=vee color=red]\n}"
-		data := []byte(codigo + conexiones)
-		_ = ioutil.WriteFile("Grafica.dot", data, 0644)
-		path, _ := exec.LookPath("dot")
-		cmd, _ := exec.Command(path, "-Tpdf", "Grafica.dot").Output()
-		_ = ioutil.WriteFile("Grafica.pdf", cmd, os.FileMode(0777))
-	} else {
-		fmt.Println("No se pudo graficar, la lista está vacía")
-	}
-
-}*/
